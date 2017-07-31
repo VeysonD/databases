6db007b409ee2217d11b275e82b9cbef2c878b3d
Rick Gallegos <duulketariakan@yahoo.com>
2017-07-30 19:01:57 -0700

    "working" again

--- server/models/index.js
+++ server/models/index.js
@@ -17,7 +17,6 @@ module.exports = {
       console.log('model post messages test');
       var messageQuery = `insert into messages(text, roomname) values ('${params}', '${param2}')`;
       //var messageQuery ="insert into messages(message) values ('" + params + "')";
-////
       //console.log('messageQuery', messageQuery);
       db.query(messageQuery, function(err, data) {
         callback(err, data);
@@ -37,7 +36,7 @@ module.exports = {
       //console.log('post user model test');
       //INSERT INTO tails (name, color) VALUES ('Rick', 'Blue'),
       var userQuery = `insert into users(username) values('${params}')`;
-      db.query(userQuery, function(err, data){
+      db.query(userQuery, function(err, data) {
         //console.log('data', data);
         callback(err, data);
       });
