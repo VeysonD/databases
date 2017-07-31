3ce8f5e09546e93c231e305add4ec69a75e324bc
Rick Gallegos <duulketariakan@yahoo.com>
2017-07-30 18:54:39 -0700

    Everything "works"

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,40 +4,60 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) {
+      //console.log('controller messages get', req);
+      models.messages.get(function(err, data) {
+        if (err) {
+          console.log('Wow much err', err);
+          throw err;
+        }
+        //console.log('get controller messages data', data);
+        res.json(data);
+      });
 
     }, // a function which handles a get request for all messages
     post: function (req, res) {
       console.log('post messages controller test');
+      //console.log('byebyebyebye');
 
-      var inputContent = req.body.message;
+      var inputContent = req.body.text;
       var inputRoom = req.body.roomname;
+      var inputUsername = req.body.username;
+      //console.log('post messages message', req);
 
-
-      models.messages.post(inputContent, inputRoom, function(err, data) {
+      models.messages.post(inputContent, inputRoom, inputUsername, function(err, data) {
         if (err) {
           throw err;
         }
-        res.end();
+        //users.post
+        res.sendStatus(201);
       });
 
     } // a function which handles posting a message to the database
   },
 
   users: {
-    // Ditto as above
     get: function (req, res) {
+      models.users.get(function(err, data) {
+        if (err) {
+          console.log('Controller users get error', err);
+          throw err;
+        }
+        //console.log('Controller users get data', data);
+        res.json(data);
+      });
 
     },
     post: function (req, res) {
+      //console.log('hihihihihihi');
       //console.log('post user controller test');
+      console.log('Controller users post req', req.body);
       var inputName = req.body.username;
       models.users.post(inputName, function(err, data) {
         if (err) {
           throw err;
         }
-        res.end();
+        res.sendStatus(201);
       });
     }
   }
 };
-//
--- server/models/index.js
+++ server/models/index.js
@@ -3,30 +3,29 @@ var db = require("../db");
 module.exports = {
   messages: {
     get: function(callback) {
-      // var messageQuery =
-      //   "select * from messages \
-      //   left outer join users \
-      //   on (users.id = messages.userid)";
-      // db.query(messageQuery, function(err, data) {
-      //   callback(err, data);
-      // });
+      var messageQuery =
+        "select * from messages \
+        left outer join users \
+        on (users.id = messages.userid)";
+      db.query(messageQuery, function(err, data) {
+        callback(err, data);
+      });
     }, // a function which produces all the messages
-    post: function(params, param2, callback) {
+    post: function(params, param2, param3, callback) {
         //INSERT INTO tails (name, color) VALUES ('Rick', 'Blue'),
 
-
-      //var messageQuery ="insert into messages(text) values('" + param1 + "')";
-      var messageQuery ="insert into messages(text) values ('" + params + "')";
-
+      console.log('model post messages test');
+      var messageQuery = `insert into messages(text, roomname) values ('${params}', '${param2}')`;
+      //var messageQuery ="insert into messages(message) values ('" + params + "')";
+////
       //console.log('messageQuery', messageQuery);
       db.query(messageQuery, function(err, data) {
         callback(err, data);
       });
-    } // a function which can be used to insert a message into the database//////////
+    } // a function which can be used to insert a message into the database/
   },
 
   users: {
-    // Ditto as above.
     get: function(callback) {
       var userQuery = "select username from users";
       db.query(userQuery, function(err, data) {
@@ -35,11 +34,11 @@ module.exports = {
     },
 
     post: function(params, callback) {
-      console.log('post user model test');
+      //console.log('post user model test');
       //INSERT INTO tails (name, color) VALUES ('Rick', 'Blue'),
-      var userQuery = "insert into users(username) values('" + params + "')";
+      var userQuery = `insert into users(username) values('${params}')`;
       db.query(userQuery, function(err, data){
-        console.log('data', data);
+        //console.log('data', data);
         callback(err, data);
       });
 
@@ -50,6 +49,6 @@ module.exports = {
 /*
 
 var messageQuery =
-  "insert into messages(text, roomname) values('" + param1 + "', '"+ param2 + "')";
+  "insert into messages(message, roomname) values('" + param1 + "', '"+ param2 + "')";
 
 */
